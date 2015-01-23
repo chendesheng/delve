2fce0830d6cd9cac4bd17d1e8aa5688d292a4215
chendesheng <ichendesheng@gmail.com>
2015-01-24 02:56:53 +0800

    Fix read zero size memory

--- proctl/variables_darwin_amd64.go
+++ proctl/variables_darwin_amd64.go
@@ -2,6 +2,9 @@ package proctl
 
 func (thread *ThreadContext) readMemory(addr uintptr, size uintptr) ([]byte, error) {
 	buf := make([]byte, size)
+	if size == 0 {
+		return buf, nil
+	}
 
 	_, err := readMemory(thread.Process.Pid, addr, buf)
 	if err != nil {
